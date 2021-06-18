3e70f394b0be503c082bada168aefff5e1cdbf0e
ombe <h3236516@gmail.com>
2021-06-18 13:20:58 +0900

    Banyak banyak hal

--- apaapp.py
+++ apaapp.py
@@ -1,28 +1,60 @@
 import sys
-from PySide2.QtWidgets import QApplication, QDialog, QLineEdit, QPushButton, QVBoxLayout
+from PySide2.QtCore import QEvent, Qt
+from PySide2.QtWidgets import (
+    QApplication,
+    QHBoxLayout,
+    QLabel,
+    QListWidget,
+    QListWidgetItem,
+    QPushButton,
+    QVBoxLayout,
+    QWidget,
+)
 
 
-class Form(QDialog):
+class Widget(QWidget):
     def __init__(self, parent=None):
-        super(Form, self).__init__(parent)
+        super(Widget, self).__init__(parent)
 
-        self.edit = QLineEdit("nama")
-        self.tombol = QPushButton("tombol")
+        menu_widget = QListWidget()
+        for i in range(10):
+            barang = QListWidgetItem(f"barang {i}")
+            barang.setTextAlignment(Qt.AlignCenter)
+            menu_widget.addItem(barang)
 
-        layout = QVBoxLayout()
-        layout.addWidget(self.edit)
-        layout.addWidget(self.tombol)
+        teksnya_widget = QLabel("seks")
+        teksnya_widget.setAlignment(Qt.AlignCenter)
+        tombol = QPushButton("sesuatu")
 
+        content_layout = QVBoxLayout()
+        content_layout.addWidget(teksnya_widget)
+        content_layout.addWidget(tombol)
+        utama_widget = QWidget()
+        utama_widget.setLayout(content_layout)
+
+        layout = QHBoxLayout()
+        layout.addWidget(menu_widget, 1)
+        layout.addWidget(utama_widget, 4)
         self.setLayout(layout)
 
-        self.tombol.clicked.connect(self.halo)
+        self.setWindowTitle("judul")
 
-    def halo(self):
-        print(f"Halo {self.edit.text()}")
+        self.resize(800, 300)
+
+    def keyPressEvent(self, event):
+        print(event)
+        if event.key() == Qt.Key_Escape:
+            self.close()
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    form = Form()
-    form.show()
+
+    w = Widget()
+    w.show()
+
+    with open("gaya.qss", "r") as f:
+        gaya = f.read()
+        app.setStyleSheet(gaya)
+
     sys.exit(app.exec_())
