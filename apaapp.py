3ea15a27ce2b582cd9d70b81d416ca3f424ad66e
ombe <h3236516@gmail.com>
2021-06-18 12:01:57 +0900

    고추가 확짖ㄴ자

--- apaapp.py
+++ apaapp.py
@@ -1,11 +1,24 @@
 import sys
-from PySide2.QtWidgets import QApplication, QDialog, QLineEdit, QPushButton
+from PySide2.QtWidgets import QApplication, QDialog, QLineEdit, QPushButton, QVBoxLayout
 
 
 class Form(QDialog):
     def __init__(self, parent=None):
         super(Form, self).__init__(parent)
-        self.setWindowTitle("eeehwe")
+
+        self.edit = QLineEdit("nama")
+        self.tombol = QPushButton("tombol")
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.edit)
+        layout.addWidget(self.tombol)
+
+        self.setLayout(layout)
+
+        self.tombol.clicked.connect(self.halo)
+
+    def halo(self):
+        print(f"Halo {self.edit.text()}")
 
 
 if __name__ == "__main__":
